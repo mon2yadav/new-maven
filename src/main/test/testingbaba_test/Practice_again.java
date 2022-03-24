0e335931133c29e8c96f2d35502abaa23af53dff
montu yadav <mon2yadav@gmail.com>
2022-03-24 16:16:30 +0530

    'add'

--- src/main/test/testingbaba_test/Practice_again.java
+++ src/main/test/testingbaba_test/Practice_again.java
@@ -13,8 +13,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -26,103 +24,102 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Practice_again extends Baseliberary {
 
-	
-
-	public static void getluch(String url)
+	public  static void getscreenshot()
 	{
+		try {
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File src=ts.getScreenshotAs(OutputType.FILE);
+		String screenshotName=null;
+		File dest=new File("./screenshot"+screenshotName+".PNG");
+		Files.copy(src, dest);
+		
+		
+		} 
+		catch (Exception e)
+		{
+			System.out.println("Issue is take screenshot  "+e);
+		}
+	}
+	public  void getlaunch(String url)
+	{
+		System.setProperty("webdriver.chrome.driver","");
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.get(url);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		driver.switchTo().frame("");
-		driver.switchTo().alert().accept();
-		driver.switchTo().alert().dismiss();
-		driver.switchTo().alert().getText();
-		driver.findElement(By.xpath("")).click();
-		driver.findElement(By.xpath("")).clear();
-		driver.findElement(By.xpath("")).sendKeys("montu");
-		driver.getTitle();
-		driver.get(url);
-		driver.close();
-		driver.quit();
-		
-		try {
-			TakesScreenshot ts=(TakesScreenshot)driver;
-			File src=ts.getScreenshotAs(OutputType.FILE);
-			String screenshotName=null;
-			File dest=new File("./screenshot"+screenshotName+".PNG");
-			Files.copy(src, dest);
-			
-			
-		} catch (Exception e) {
-			System.out.println("Issue in take screenshot  "+e);
+		driver.manage().timeouts().implicitlyWait(10 ,TimeUnit.SECONDS);
 		}
-		
-	}
-	public void doubleclick()
+	
+	public void doubleclick(WebElement ele )
 	{
 		Actions act=new Actions(driver);
 		act.doubleClick().perform();
-	}
-	
-	public void rightclick() {
+		
 		Actions act1=new Actions(driver);
-		act1.contextClick();
+		act1.contextClick().perform();
+		
+		Actions act2=new Actions(driver);
+		act2.moveToElement(ele, 0, 0);
+		
 		
 	}
-	public void switchtab()
+	
+	public void getmethod(String url)
 	{
+		driver.getTitle();
+		driver.switchTo().alert().getText();
+		driver.get(url);
+		driver.findElement(By.xpath("")).click();
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys("montu");
+		driver.findElement(By.xpath("")).sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("")).sendKeys(Keys.F5);
+		driver.navigate().refresh();
+		driver.navigate().forward();
+		driver.navigate().back();
 		driver.switchTo().defaultContent();
-		driver.switchTo().frame(2);
 	}
 	public void changewindow(int tabindex)
 	{
 		Set<String> tab=driver.getWindowHandles();
 		ArrayList<String> tabs=new ArrayList<String>(tab);
+		
 		driver.switchTo().window(tabs.get(tabindex));
-	}
-	public void scrollxpath()
+		
+		}
+	public void scrolbyxpath()
 	{
 		try {
 			JavascriptExecutor jse=(JavascriptExecutor)driver;
-			jse.executeScript(null, null);
+			jse.executeScript("",By.xpath(""));
 		} catch (Exception e) {
-			System.out.println(" issue in scrollbexpath  "+e);
+			System.out.println("issue in scrollbyxpath  "+e);
 		}
-		
 	}
+	
 	public void dynamicwait()
 	{
-		try
-		{
-			WebDriverWait wait=new WebDriverWait(driver, 200);
-			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("")));
-	} 
-		catch (Exception e) {
-		System.out.println("issue in dynamic wait");
+		WebDriverWait wait=new WebDriverWait(driver, 100);
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("")));
+		
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
 	}
 	
-			
-	}
-	
-	public static void dropdownbyvalue(WebElement ele, String value)
+	public void dropdownbyvalue(WebElement ele, String value)
 	{
 		Select sel=new Select(ele);
 		sel.selectByVisibleText(value);
 	}
-	public void moveforward()
+	public void alert()
 	{
-		driver.navigate().forward();
+		driver.switchTo().alert();
+		driver.switchTo().alert().dismiss();
+		driver.switchTo().alert().getText();
+		driver.switchTo().frame("{frameindex/frameName/frameId}");
+		
+		
 	}
-	public void movebackword()
-	{
-		driver.navigate().back();
-	}
-	public void refreshbrowser()
-	{
-		driver.navigate().refresh();                                      // for refresh the browser
-		driver.findElement(By.xpath("")).sendKeys(Keys.F5);                //  for refresh the browser
-		driver.findElement(By.xpath("")).sendKeys(Keys.ENTER);                      // for send data and press enter
-	}
+	
+	
 }
