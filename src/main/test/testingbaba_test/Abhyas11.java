2e5ed49465f84a0e154ac83254eecdfcc3df810f
montu yadav <mon2yadav@gmail.com>
2022-06-28 13:38:59 +0530

    'add'

--- src/main/test/testingbaba_test/Abhyas11.java
+++ src/main/test/testingbaba_test/Abhyas11.java
@@ -28,6 +28,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -40,8 +42,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Abhyas11 extends Baseliberary{
 	
-	public void takescreenshot()
-	{
+	public void takescreenshot() {
 		try {
 			TakesScreenshot ts=(TakesScreenshot)driver;
 			File src=ts.getScreenshotAs(OutputType.FILE);
@@ -54,19 +55,29 @@ public class Abhyas11 extends Baseliberary{
 		}
 	}
 	
-	public void driverlounch(String ulr)
-	{
+	public void driverlounch(String url) {
 		System.setProperty("webdriver.chrome.driver", "");
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
-		driver.get(ulr);
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebDriverManager.iedriver().setup();
+		driver=new InternetExplorerDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+		driver.get(url);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	}
 	
-	public void doubleclick()
-	{
+	public void clickmethod() {
 		Actions act=new Actions(driver);
 		act.doubleClick().perform();
 		act.contextClick().perform();
@@ -74,7 +85,7 @@ public class Abhyas11 extends Baseliberary{
 		act.dragAndDropBy(null, 0, 0).build().perform();
 	}
 	
-	public void somedrivermethod() {
+	public void drivermethod(String url) {
 		driver.findElement(By.xpath("")).clear();
 		driver.findElement(By.xpath("")).click();
 		driver.findElement(By.xpath("")).sendKeys("montu");
@@ -88,13 +99,13 @@ public class Abhyas11 extends Baseliberary{
 		driver.switchTo().alert().getText();
 		driver.switchTo().frame("{frameName/frameId/frameIndex}");
 		driver.switchTo().defaultContent();
+		driver.get(url);
+		driver.close();
 		driver.getTitle();
-		driver.getClass();
-		driver.get("");
-		}
+		driver.quit();
+	}
 	
-	public void changewindow(int tabindex)
-	{
+	public void changewindow(int tabindex) {
 		Set<String> tab = driver.getWindowHandles();
 		ArrayList<String> tabs=new ArrayList<String>(tab);
 		driver.switchTo().window(tabs.get(tabindex));
@@ -105,39 +116,38 @@ public class Abhyas11 extends Baseliberary{
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("")));
 	}
 	
-	public void dropdownbyvalue(WebElement ele, String value)
-	{
+	public void dropdownbyvalue(WebElement ele, String value) {
 		Select sel=new Select(ele);
 		sel.selectByVisibleText(value);
-	}
+		sel.selectByIndex(0);
+		sel.selectByValue(value);
+		}
 	
-	public String getcurrentdateandtime()
-	{
+	public String getcurrentdateandtime() {
 		String datetime=null;
 		try {
 			DateFormat df=new SimpleDateFormat("dd-mm-yyyy  hh:mm");
-			Date dateobj=new Date(11);
+			Date dateobj=new Date();
 			df.format(dateobj);
 			datetime=df.format(dateobj);
+			
 		} catch (Exception e) {
 			System.out.println("Issue in get current date and time  "+e);
 		}
 		return datetime;
 	}
 	
-	public void scrollbyxapth()
-	{
+	public void scrollbyxpath() {
 		try {
 			JavascriptExecutor jse=(JavascriptExecutor)driver;
-			jse.executeScript("",By.xpath(""));
+			jse.executeScript("", By.xpath(""));
 			
 		} catch (Exception e) {
-			System.out.println("issue in scroll by xpath  "+e);
+			System.out.println("Issue in scrol by xpath  "+e);
 		}
 	}
 	
-	public String propertyutility(String key)
-	{
+	public String propertyutility(String key) {
 		String path="";
 		String value="";
 		try {
@@ -147,69 +157,75 @@ public class Abhyas11 extends Baseliberary{
 			value=prop.getProperty(key).trim();
 			
 		} catch (Exception e) {
-			System.out.println("Issue in get read data from property utility  "+e);
+			System.out.println("Issue in get read data from property utility "+e);
 		}
 		return value;
+		
 	}
 	
-	public static void brokenlink()
-	{
+	
+	public void brokenlink() {
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.get("https://www.cryptoknowmics.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		List<WebElement> counts = driver.findElements(By.tagName("a"));
-		System.out.println("Number of Links "+counts.size());
+		System.out.println("Number of links "+counts.size());
 		ArrayList<String> urlList=new ArrayList<String>();
-		for(WebElement e:counts)
-		{
+		for(WebElement e:counts) {
 			String url = e.getAttribute("href");
 			System.out.println(urlList.add(url));
 			checkBrokenLink(url);
 		}
-		driver.quit();
-		
 	}
 
-	private static void checkBrokenLink(String urlList) {
+	private void checkBrokenLink(String urlList) {
 		try {
 			URL urls=new URL(urlList);
 			HttpURLConnection httpcon=(HttpURLConnection)urls.openConnection();
 			httpcon.setConnectTimeout(1000);
 			httpcon.connect();
-			if(httpcon.getResponseCode()>=400)
-			{
-				System.out.println(urlList+httpcon.getResponseMessage()+"This link is broken");
+			if(httpcon.getResponseCode()>=100 && httpcon.getResponseCode()<200) {
+				System.out.println(urlList +"-----> " +httpcon.getResponseMessage()+" This link is broken with informational error");
 			}
-			else
-			{
-				System.out.println(urlList+httpcon.getResponseMessage()+"This link is not broken");
+			else if(httpcon.getResponseCode()>=300 && httpcon.getResponseCode()<400) {
+				System.out.println(urlList + "----> " +httpcon.getResponseMessage() +"This link is broken with Redirection error");
 			}
-			
+			else if(httpcon.getResponseCode()>=400 && httpcon.getResponseCode()<500) {
+				System.out.println(urlList +"------> " +httpcon.getResponseMessage()+ "This link is broken with client error");
+			}
+			else if(httpcon.getResponseCode()>=500 && httpcon.getResponseCode()<600) {
+				System.out.println(urlList +"----->" +httpcon.getResponseMessage()+"This link is broken with internal server error");
+			}
+			else {
+				System.out.println(urlList+"---->"+httpcon.getResponseMessage()+"This link is NOT Broken");
+			}
 			
 		} catch (Exception e) {
-			System.out.println("issue in check broken link  "+e);
+			System.out.println("Issue in check broken link  "+e);
 		}
 		
+		
+		
 	}
-	
-	public int  getrandomnumber() {
+	public int getrandomnumber() {
 		int randomNumber=0;
 		try {
 			Random objgenerator=new Random();
-			for(int i=0; i<=10; i++)
-			{
+			for(int i=0; i<=10; i++) {
 				randomNumber=objgenerator.nextInt(1000);
 			}
 			
 		} catch (Exception e) {
-			System.out.println("Issue in get random number  "+e);
+			System.out.println("Issue in get random Number "+e);
 		}
 		return randomNumber;
 	}
 	
-	public void uploadfilewithrobot(String imagepath) {
+	
+	public void uploadfilewithrobotclass(String imagepath) {
+		
 		StringSelection stringSelection=new StringSelection(imagepath);
 		Clipboard clipboard=Toolkit.getDefaultToolkit().getSystemClipboard();
 		clipboard.setContents(stringSelection, null);
@@ -219,10 +235,11 @@ public class Abhyas11 extends Baseliberary{
 			robot.keyPress(KeyEvent.VK_ENTER);
 			robot.keyRelease(KeyEvent.VK_ENTER);
 			robot.delay(100);
+			
 		} catch (Exception e) {
 			System.out.println("Issue in upload file with robot class  "+e);
 		}
-		
+	robot.delay(150);	
 	}
 	
 	public String excelutility(int rownum, int columnum) {
@@ -235,25 +252,28 @@ public class Abhyas11 extends Baseliberary{
 			value=sheet.getRow(rownum).getCell(columnum).getStringCellValue();
 			
 		} catch (Exception e) {
-			System.out.println("Issue in get read data from excell utility  "+e);
+			System.out.println("Issue in get read data from excel utility  "+e);
 		}
 		return value;
 	}
 	
-	public int rowcount()
-	{
+	public int rowcount() {
 		String path="";
-		int lastrowcount=0;
+		int lastrowNumber=0;
 		try {
 			FileInputStream fis=new FileInputStream(path);
 			XSSFWorkbook wb=new XSSFWorkbook();
 			XSSFSheet sheet=wb.getSheetAt(0);
-			lastrowcount=sheet.getLastRowNum();
+			lastrowNumber=sheet.getLastRowNum();
 			
 		} catch (Exception e) {
-			System.out.println("Issue in get last row count  "+e);
+			System.out.println("Issue in get last row number  "+e);
 		}
-		return lastrowcount;
+		return lastrowNumber;
 	}
+	
+	
+	
+	
 }
 
